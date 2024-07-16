597b67ffce38cfa422ca293b25d5edf35af42a8a
PC <hyuk_9@entropykorea.com>
2024-07-16 23:49:49 +0900

    [feature] Add file size exception

--- src/main/java/com/example/rsupport/noticeboard/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/rsupport/noticeboard/common/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +18,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         Map<String, String> errors = new HashMap<>();
         e.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, String>> handleMaxSizeException(MaxUploadSizeExceededException e) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "파일 크기가 너무 큽니다.");
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
